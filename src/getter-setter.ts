4b757fafc8f8cda5c4871c453ae35d0dd80c76e4
Muhammad SA <MuhammadSA211999>
2023-05-14 01:48:29 +0600

    get and set function

---
+++ src/getter-setter.ts
@@ -0,0 +1,2 @@
+//getter:*get* keyword diye suru hoy, class er kuno private property get function e bebohar kora hole sei property instance/object call kore sorasori get kora zay. kuno function call er proujon pore nah.
+//setter:*set* keyword diye suru hoy, class er kuno private property set function e bebohar kora hole sei property instance/object call kore sorasori set kora zay. kuno function call er proujon pore nah.
